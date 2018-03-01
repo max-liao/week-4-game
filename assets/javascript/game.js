15bfbad40ea6a5cf069279a86cc129da3462468b
Max Liao <max.liao@gmail.com>
2018-02-28 21:55:37 -0500

    Recommended Done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,50 +4,88 @@ alert("Press any key to get started!");
 var game = {
 
     //variables
-    random: 0,
+    initial: 0, 
     userscore: 0,
-    computerscore: 0,
     name: "hangman",
     creator: "Max",
+    heartval: 0,
+    spadeval: 0,
+    clubval: 0,
+    diamondval: 0,
     
     //functions
     hellow : function(){
       console.log("Hello World");
     },
+
+    buttons: function(){
+      var btnvalue = Math.floor(Math.random()*9 +1);
+      return btnvalue;
+    },
     
     start: function(){
       var initialnum = Math.floor(Math.random()*100);
-      console.log("Creating random number");
+      game.userscore = 0;
+      console.log("Creating initial random numbers");
+      console.log("Player must make a score of " + initialnum);
+      game.heartval = game.buttons();
+      game.spadeval = game.buttons();
+      game.clubval = game.buttons();
+      game.diamondval = game.buttons();
+      $("#userscoredisplay").text("User Score");
+      $("#scoredisplay").text("Player must make a score of " + initialnum);
+      // display = document.getElementById('scoredisplay');
+      // display.lastChild.innerHTML = "new";
       return initialnum;
     },
 
     heart: function(){
-      var initialnum = Math.floor(Math.random()*100);
-      console.log("Creating random number");
-      return initialnum;
-    }
-    ,
-
-    : function(){
-      var initialnum = Math.floor(Math.random()*100);
-      console.log("Creating random number");
-      return initialnum;
+      game.userscore += game.heartval;
+      console.log("Adding one heart");
+      game.wincheck();
     },
 
-    start: function(){
-      var initialnum = Math.floor(Math.random()*100);
-      console.log("Creating random number");
-      return initialnum;
+    spade: function(){
+      game.userscore += game.spadeval;
+      console.log("Adding one spade");
+      game.wincheck();
+    },
+
+    club: function(){
+      game.userscore += game.clubval;
+      console.log("Adding one club");
+      game.wincheck();
+    },
+
+    diamond: function(){
+      game.userscore += game.diamondval;
+      console.log("Adding one diamond");
+      game.wincheck();
+    },
+
+    wincheck: function(){
+      console.log(game.userscore);
+      $("#userscoredisplay").text("Current player score " + game.userscore);
+      if (game.userscore == game.initial)
+      {
+        // console.log("WINNER! Restarting game");
+        alert("WINNER! Restarting game")
+        game.initial = game.start();
+      }
+      else if (game.userscore > game.initial)
+      {
+        // console.log("OVERLOAD! Restarting game")
+        alert("OVERLOAD! Restarting game")
+        game.initial = game.start();
+      }
     }
 }
 
-
 // Initialize game
-game.random = game.start();
-console.log(game.random);
+game.initial = game.start();
 
 // Event Listener
-document.getElementById("heartbtn").addEventListener("click", game.hellow);
-document.getElementById("heartbtn").addEventListener("click", game.hellow);
-document.getElementById("heartbtn").addEventListener("click", game.hellow);
-document.getElementById("heartbtn").addEventListener("click", game.hellow);
+document.getElementById("heartbtn").addEventListener("click", game.heart);
+document.getElementById("spadebtn").addEventListener("click", game.spade);
+document.getElementById("clubbtn").addEventListener("click", game.club);
+document.getElementById("diamondbtn").addEventListener("click", game.diamond);
